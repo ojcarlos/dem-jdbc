0740be93145f8c8634eac528854e347318f9291d
ojcarlos <carlos.ojunior00@gmail.com>
2023-01-18 15:56:28 -0400

    sellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,10 +9,10 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        //Connection conn = DB.getConnection();
-        //DB.closeConnection();
-       // Department obj = new Department(1, "Books");
-       // Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-       // System.out.println(seller);
+
+       Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println(seller);
     }
 }
